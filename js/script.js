c8f465c669288e190cfd5ee31e30130a3844c7d6
imoncn <service@yimmr.com>
2023-08-24 14:02:18 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -36,6 +36,8 @@ document.addEventListener('DOMContentLoaded', function () {
             arSystem.stop();
         });
         switchCameraButton('#switch-camera-button', arSystem);
+        takePhotoButton('#take-photo-button');
+        recordVideoButton('#record-photo-button');
     });
 
     function switchCameraButton(selector, arSystem) {
@@ -46,5 +48,112 @@ document.addEventListener('DOMContentLoaded', function () {
         el?.addEventListener('click', function () {
             arSystem.switchCamera();
         });
+    }
+
+    function takePhotoButton(selector) {
+        const el = document.querySelector(selector);
+        el?.addEventListener('click', takePhoto);
+    }
+
+    function recordVideoButton(selector) {
+        const el = document.querySelector(selector + '');
+        if (!el) return;
+        const rawText = el.textContent;
+        const mediaRecorder = createVideoRecorder();
+        el.addEventListener('click', function () {
+            if (mediaRecorder.state === 'recording') {
+                mediaRecorder.stop();
+                el.textContent = 'Stop Recording';
+            } else {
+                mediaRecorder.start();
+                el.textContent = rawText;
+            }
+        });
+    }
+
+    function takePhoto(scene) {
+        const video = document.querySelector('video');
+
+        if (video == null) return;
+
+        const filename = 'photo.png';
+
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        let width = video.clientWidth * 2;
+        let height = video.clientHeight * 2;
+        canvas.width = width;
+        canvas.height = height;
+        const videoStyle = window.getComputedStyle(video);
+        const top = videoStyle.getPropertyValue('top');
+
+        video.pause();
+        context.drawImage(video, 0, parseFloat(top), width, height);
+        context.drawImage(
+            scene.components.screenshot.getCanvas('perspective'),
+            0,
+            0,
+            width,
+            height
+        );
+
+        if (window.navigator.msSaveOrOpenBlob) {
+            var blobObject = canvas.msToBlob();
+            window.navigator.msSaveOrOpenBlob(blobObject, filename);
+        } else {
+            const link = document.createElement('a');
+            link.href = canvas.toDataURL('image/png');
+            link.download = filename;
+            link.click();
+        }
+
+        video.play();
+    }
+
+    function createVideoRecorder() {
+        const video = document.querySelector('video');
+
+        if (video == null) return;
+
+        const filename = 'video.mp4';
+        const fileType = 'video/mp4';
+
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        let width = video.clientWidth;
+        let height = video.clientHeight;
+        canvas.width = width;
+        canvas.height = height;
+
+        const mediaRecorder = new MediaRecorder(canvas.captureStream(), { mimeType: fileType });
+        const chunks = [];
+
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            video.srcObject = stream;
+            video.play();
+            function render() {
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                requestAnimationFrame(render);
+            }
+            render();
+        });
+
+        // 监听录制数据事件
+        mediaRecorder.ondataavailable = function (event) {
+            chunks.push(event.data);
+        };
+
+        // 监听录制结束事件
+        mediaRecorder.onstop = function () {
+            const videoBlob = new Blob(chunks, { type: fileType });
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(videoBlob);
+            link.download = filename;
+            link.click();
+        };
+
+        mediaRecorder.onstart = function () {};
+
+        return mediaRecorder;
     }
 });
--- views/face.php
+++ views/face.php
@@ -10,8 +10,11 @@
 </a-scene>
 <div id="ar-controls-container" class="ar-controls-container">
     <div class="buttons">
+        <button id="take-photo-button">Take Photo</button>
+        <button id="record-video-button">Record</button>
         <button id="start-button">Start</button>
         <button id="stop-button">Stop</button>
         <button id="switch-camera-button">Switch Camera</button>
     </div>
 </div>
+<canvas id="media-canvas"></canvas>
