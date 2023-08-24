8232fa85dc5c3951beb4ff01434fe5937c73451e
imoncn <service@yimmr.com>
2023-08-24 16:03:05 +0800

    first commit

--- views/face.php
+++ views/face.php
@@ -1,12 +1,9 @@
-<a-scene mindar-face embedded color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights"
+<a-scene mindar-face color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights"
          vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
-    <a-assets>
-        <a-asset-item id="glassesModel"
-                      src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/face-tracking/assets/glasses/scene.gltf"></a-asset-item>
-    </a-assets>
-    <a-camera active="false" position="0 0 0"></a-camera>
-    <a-entity mindar-face-target="anchorIndex: 168">
-        <a-gltf-model rotation="0 0 0" position="0 0 0" scale="0.01 0.01 0.01" src="#glassesModel"></a-gltf-model>
+    <?php require_once __DIR__.'/assets.php'; ?>
+    <a-camera position="0 0 0"></a-camera>
+    <a-entity mindar-face-target="anchorIndex: 1">
+        <a-sphere color="green" radius="0.1"></a-sphere>
     </a-entity>
 </a-scene>
 <div id="ar-controls-container" class="ar-controls-container">
