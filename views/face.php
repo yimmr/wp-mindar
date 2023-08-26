20deea9dc31583d23a17f6ba4fb51cb5c47d7087
imoncn <service@yimmr.com>
2023-08-26 14:27:33 +0800

    first commit

--- views/face.php
+++ views/face.php
@@ -1,7 +1,7 @@
 <a-scene mindar-face embedded color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights"
          vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
     <?php require_once __DIR__.'/assets.php'; ?>
-    <a-camera active="false" position="0 0 0"></a-camera>
+    <a-camera position="0 0 0"></a-camera>
     <?php foreach ($pageData['items'] as $item) {?>
     <a-entity
               mindar-face-target="anchorIndex: <?php echo $item['anchorIndex'] ?? 1; ?>">
