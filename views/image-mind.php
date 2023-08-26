6fa6d3c2de11a4b635de91baa715e3b573ebe02d
imoncn <service@yimmr.com>
2023-08-26 13:56:37 +0800

    first commit

--- views/image-mind.php
+++ views/image-mind.php
@@ -1,9 +1,11 @@
-<a-scene mindar-image="imageTargetSrc: <?php echo $pageData['target_src']; ?>"
-         vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+<a-scene mindar-image="imageTargetSrc: <?php echo $pageData['target_src']; ?>;"
+         color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false"
+         device-orientation-permission-ui="enabled: false">
     <?php require_once __DIR__.'/assets.php'; ?>
     <?php foreach ($pageData['items'] as $item) {?>
     <a-entity
               mindar-face-target="targetIndex: <?php echo $item['targetIndex'] ?? 0; ?>">
+        <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
         <?php echo $arPage->buildObjectHTML($item['object']); ?>
     </a-entity>
     <?php }?>
